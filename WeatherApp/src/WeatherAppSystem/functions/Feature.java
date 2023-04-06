fb126f088e07d8ef8e52e8782fcb1cf8e40c1542
golamt313 <gj4559@wayne.edu>
2023-04-05 21:32:53 -0400

    Optimized via AI

--- WeatherApp/src/WeatherAppSystem/functions/Feature.java
+++ WeatherApp/src/WeatherAppSystem/functions/Feature.java
@@ -15,115 +15,88 @@ import org.json.simple.parser.ParseException;
 
 public class Feature {
 
-	public JSONObject optionA(Scanner scan) throws FileNotFoundException {
-
-		System.out.println("Please enter the name of a city:");
-		String input = optionC(scan);
-		// If the city name is empty or is null, we ask them again to input a city name
-		if (input != "" && input != null) {
-			//input = input.toUpperCase();
-			System.out.println("You've selected " + input + "!");
-
-			try {
-				// In the future we need to validate whether the input is a valid city
-				String geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + input
-						+ ",826&limit=1&appid=eab646dbd2d8af731b95792030c4d645";
-				ApiReader reader = new ApiReader(geoApi);
-
-				// This represents the response of the json data from the api
-				String jsonResponse = reader.connect();
-				// Create parser to convert string to objects/array
-				JSONParser parser = new JSONParser();
-				// Making response as a json object
-				JSONArray jsonArray = (JSONArray) parser.parse(jsonResponse);
-				// This represents the city's properties
-				JSONObject citydata = (JSONObject) jsonArray.get(0);
-				Double lat = (Double) citydata.get("lat");
-				Double lon = (Double) citydata.get("lon");
-
-				String dataApi = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon
-						+ "&exclude=minutely,hourly&appid=eab646dbd2d8af731b95792030c4d645";
-				reader.setAPI_URL(dataApi);
-				jsonResponse = reader.connect();
-				// Making response as a json object
-				JSONObject weatherData = (JSONObject) parser.parse(jsonResponse);
-
-				return weatherData;
-			} catch (Exception e) {
-				System.out.println(e);
-			}
-
-		} else {
-			System.err.println("Incorrect input, please try again!");
-			optionA(scan);
-		}
+	public JSONObject optionA(Scanner scan) throws IOException, ParseException {
+	    System.out.println("Please enter the name of a city:");
+	    String input = optionC(scan);
+	    if (input == null || input.isEmpty()) {
+		System.err.println("Incorrect input, please try again!");
 		return null;
+	    }
+
+	    String geoApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + input
+		    + ",826&limit=1&appid=eab646dbd2d8af731b95792030c4d645";
+	    String dataApi = "https://api.openweathermap.org/data/2.5/onecall?%s&exclude=minutely,hourly&units=metric&appid=eab646dbd2d8af731b95792030c4d645";
+
+	    try (ApiReader reader = new ApiReader(geoApi)) {
+		JSONParser parser = new JSONParser();
+		JSONObject cityData = (JSONObject) parser.parse(reader.connect());
+		Double lat = (Double) cityData.get("lat");
+		Double lon = (Double) cityData.get("lon");
+		dataApi = String.format(dataApi, "lat=" + lat + "&lon=" + lon);
+	    } catch (IOException | ParseException e) {
+		System.err.println(e);
+		return null;
+	    }
+
+	    try (ApiReader reader = new ApiReader(dataApi)) {
+		JSONParser parser = new JSONParser();
+		return (JSONObject) parser.parse(reader.connect());
+	    } catch (IOException | ParseException e) {
+		System.err.println(e);
+		return null;
+	    }
 	}
 
-	public void optionB(Scanner scan ,JSONObject weatherData) {
-		JSONObject current = (JSONObject) weatherData.get("current");
-		
-		System.out.println("Weather report: ");
-		
-		//date of weather report  // the api provides date in unix time so it must be converted
-		Date convertedDate = new Date((long)current.get("dt")*1000); // * by 1000 because api gives it in seconds while java requires milliseconds
-		
-		System.out.println("Date: " + convertedDate);
-		
-		//current temperature
-		//   uses -273.15 to convert from kelvin to celsius
-		System.out.printf("Current Temperature: %.2f °C  :  Feels like: %.2f °C \n", ((Double) current.get("temp")-273.15)
-				, ((Double) current.get("feels_like")-273.15));
-		
-		//wind speed and direction
-		System.out.println("Wind speed: " + current.get("wind_speed") + " Wind direction: " + current.get("wind_deg") + " Degrees");
-		
-		// Feature D extended weather report
-		// current humidity
-		System.out.println("Relative Humidity: " + current.get("humidity") + "%");
-		
-		// visibility
-		System.out.println("visibility: " + current.get("visibility") + " metres");
-		
-		//weather summary
-		JSONArray weather = (JSONArray) current.get("weather");
-		JSONObject summary = (JSONObject) weather.get(0);
-		
-		System.out.println("Weather type: " + summary.get("main"));
-		System.out.println("Weather description: " + summary.get("description")); 
-		
-		System.out.println("---Press enter to return to main menu---");
-		scan.nextLine();
+	public void optionB(Scanner scan, JSONObject weatherData) {
+	    // Extract the current weather data and weather summary from the JSON object
+	    JSONObject current = weatherData.getJSONObject("current");
+	    JSONArray weather = current.getJSONArray("weather");
+	    JSONObject summary = weather.getJSONObject(0);
+
+	    // Print the weather report
+	    System.out.println("Weather report:");
+
+	    // Print the date of the weather report
+	    System.out.printf("Date: %s\n", new Date(current.getLong("dt") * 1000));
+
+	    // Print the current temperature and feels-like temperature in Celsius
+	    System.out.printf("Current Temperature: %.2f °C : Feels like: %.2f °C\n", current.getDouble("temp") - 273.15,
+		    current.getDouble("feels_like") - 273.15);
+
+	    // Print the wind speed and direction in meters per second and degrees, respectively
+	    System.out.printf("Wind speed: %.1f m/s Wind direction: %d degrees\n", current.getDouble("wind_speed"),
+		    current.getInt("wind_deg"));
+
+	    // Print the relative humidity as a percentage and the visibility in meters
+	    System.out.printf("Relative Humidity: %d%%\nVisibility: %d metres\n", current.getInt("humidity"),
+		    current.getInt("visibility"));
+
+	    // Print the weather type and description
+	    System.out.printf("Weather type: %s\nWeather description: %s\n", summary.getString("main"),
+		    summary.getString("description"));
+
+	    // Prompt the user to return to the main menu
+	    System.out.println("---Press enter to return to main menu---");
+	    scan.nextLine();
 	}
 
 	public String optionC(Scanner scan) throws FileNotFoundException {
-		Scanner file = new Scanner(new FileReader("city.txt")); // this reads the file from the current working directory which needs to be changed
-		ArrayList<String> city = new ArrayList<>();
-		ArrayList<String> newList = new ArrayList<>();
+	    ArrayList<String> cities = new ArrayList<>();
+	    try (Scanner file = new Scanner(new FileReader("city.txt"))) {
 		while (file.hasNext()) {
-			city.add(file.next().toUpperCase());
+		    cities.add(file.next().toUpperCase());
 		}
-		
-		String input = scan.nextLine().toUpperCase();
-		if (input.length() == 3) {
-			for (int i = 0; i < city.size(); i++) {
-				if (input.subSequence(0, 3).equals(city.get(i).subSequence(0, 3))) {
-					newList.add(city.get(i));
-				}
-			}
-			if (newList.size() > 1) {
-				for (int i = 0; i < newList.size(); i++) {
-					System.out.println(newList.get(i));
-				}
-				System.out.println("please can you specified the city");
-				input = scan.next().toUpperCase();
-			}else {
-				input = newList.get(0);
-			}
-			
-		}
-		file.close();
-		return input;
+	    }
+
+	    String input = scan.nextLine().toUpperCase();
+	    if (input.length() == 3) {
+		cities.stream()
+			.filter(city -> city.startsWith(input))
+			.forEach(System.out::println);
+		System.out.println("Please specify the city:");
+		input = scan.next().toUpperCase();
+	    }
+	    return input;
 	}
 
 }
